45f2da17209f98266b825f07c3f1f16f728b5c07
AnilMula <anilreddymula@gmail.com>
2022-02-27 21:36:44 +0100

    week1 homework

---
+++ week1/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string input = "Intellectualization";
+int vowelCount = GetVowelCount(input.ToLower());
+
+int GetVowelCount(string input)
+{
+    int count = 0;
+    int inputLength = input.Length;
+    for(int i = 0; i < inputLength; i++)
+    {
+        if((input[i] == 'a' )|| (input[i] == 'e' )|| (input[i] == 'i' )|| (input[i] == 'o' )|| (input[i] == 'u' ))
+        {
+            ++count;
+        }
+    }
+    return count;
+}
+Console.WriteLine($"Number of vowels: {vowelCount}");
